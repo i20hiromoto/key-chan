4dd8f5c39b8e09a8b2e4f573a8b88f62c4c263be
Hiromoto <dnne1555@gmail.com>
2024-06-28 11:13:44 +0900

    backボタンを呼び出した後ページをリロードするようにした

--- pages/select.tsx
+++ pages/select.tsx
@@ -44,6 +44,7 @@ const Select: React.FC = () => {
                 'Content-Type': 'application/json'
               }
             });
+        router.push('/select');
     }catch(error){
         alert('An error occurred during login. Please try again.');
     } 
